3dbbf1eca5724eaa36a7512f05a2cfeb3446c184
Damien Siri <dsiri12@gmail.com>
2019-09-01 10:40:58 -0400

    fetch data from endpoint

--- src/index.js
+++ src/index.js
@@ -1,18 +1,43 @@
-import React, { useContext, useReducer } from 'react';
+import React, { useContext, useReducer, useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
+
 import TodosContext from './context';
 import todosReducer from './reducer';
+
 
 import TodoList from './components/TodoList';
 import TodoForm from './components/TodoForm';
 
+const useAPI = endpoint => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+    const getData = async () => {
+        const response = await axios.get(endpoint)
+        setData(response.data)
+    }
+
+    return data;
+}
 
 const App = () => {
     const initialState = useContext(TodosContext)
     console.log("DDD", initialState);
 
     const [state, dispatch] = useReducer(todosReducer, initialState)
+
+    const savedTodos = useAPI('https://hooks-api.dsiri12.now.sh/todos');
+
+    useEffect(() => {
+        dispatch({
+            type: "GET_TODOS",
+            payload: savedTodos
+        })
+    }, [savedTodos])
 
     return (
         <TodosContext.Provider value={{ state, dispatch }}>
@@ -26,8 +51,3 @@ ReactDOM.render(
         <App />,
     document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
