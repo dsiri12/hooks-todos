433843f327f7969e2c4fced7b2ff601de65435ab
Damien Siri <dsiri12@gmail.com>
2019-09-01 11:40:20 -0400

    add new todo

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect, useContext } from "react";
+import axios from 'axios';
+import uuidv4 from 'uuid/v4';
 import TodosContext from '../context';
 
 export default function TodoForm() {
@@ -15,12 +17,17 @@ export default function TodoForm() {
     [currentTodo.id]
 );
 
-    const handleSubmit = event => {
+    const handleSubmit = async event => {
         event.preventDefault();
         if (currentTodo.text) {
             dispatch({ type: "UPDATE_TODO", payload: todo })
         } else {
-            dispatch({ type: "ADD_TODO", payload: todo });
+            const response = await axios.post("https://hooks-api.dsiri12.now.sh/todos", {
+                id: uuidv4(),
+                text: todo,
+                complete: false
+            })
+            dispatch({ type: "ADD_TODO", payload: response.data });
         }
         setTodo("");
     }
